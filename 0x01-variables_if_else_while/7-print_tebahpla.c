a19e4fd1afffd942d6eb3a30a3e28585c190fd99
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-17 18:52:02 -0700

    new

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - using putchar 
+ *Return: 0
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+	putchar('\n');
+	return (0);
+}
