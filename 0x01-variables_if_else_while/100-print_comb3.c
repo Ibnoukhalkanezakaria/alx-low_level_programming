7db8775475fe890a090a51367e6e6e8d87ee8c76
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-17 18:25:43 -0700

    new

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - using putchar rather then print in this function
+ *Return: 0
+ */
+
+int main(void)
+{
+	int a, b;
+
+	a = 0;
+	b = 0;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (a < b)
+			{
+				putchar(48 + a);
+				putchar(48 + b);
+
+				if (a != 8)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
