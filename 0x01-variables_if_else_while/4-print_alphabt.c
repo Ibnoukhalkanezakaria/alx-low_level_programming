edc35d572de8bdc79caa15ebc9dd6e580851c5dc
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-17 19:06:12 -0700

    new

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - using if and while in this function
+ *Return:0;
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
