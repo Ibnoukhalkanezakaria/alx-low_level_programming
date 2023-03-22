7b32a4b57cfea6b1f67a2070d5ea3b9a0f1cfc5a
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-21 17:30:26 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * using loop to write all alphabet lowercase
+ * Return: the parameter
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a' ;  n <= 'z' ; n++)
+		_putchar(n);
+	_putchar('\n');
+}
