dbc33aed36a37e796c9329c5041ebf618f4d1d86
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-21 18:05:36 -0700

    new

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isalpha - using if and return to return the result
+ *@c: input
+ * Return: 0 or 1
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
