def646c1203ecff6c9ee89de960085866be1dd48
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-23 16:30:56 -0700

    new

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *mul - sum of two integers
+ *@a: first input
+ *@b: second input
+ *Return: result
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+
