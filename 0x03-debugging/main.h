9714c1d20a3f3dc807e958105cb8c1152a4f7ee5
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-23 12:41:14 -0700

    new

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _putchar(char c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-void jack_bauer(void);
-int add(int x, int y);
-void print_to_98(int n);
-void times_table(void);
-void print_times_table(int n);
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+#endif
