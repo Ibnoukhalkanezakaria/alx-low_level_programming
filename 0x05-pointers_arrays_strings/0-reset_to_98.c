5ccb008f1ce234d33e5f48e38c36902fa152eb03
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-27 15:15:44 -0700

    new

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,13 @@
-#include <stdio.h>
+#include "main.h"
 
-void reset_to_98(int *n);
-
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
+/**
+ *reset_to_98 - reset to 98
+ *@n : input
+ *Return : void
+ */
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
+
