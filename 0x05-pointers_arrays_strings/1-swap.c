dc7d6b1af545cc8ce8a436dc3aca1d5b890024a6
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-27 15:51:47 -0700

    new

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swap_int
+ * @a: first input
+ * @b: second input
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+	*b = c;
+}
+
