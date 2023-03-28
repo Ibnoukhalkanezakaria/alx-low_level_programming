f8e7d0f8880e7a3a4b51f1da6504a2796025d00f
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-27 18:47:02 -0700

    new

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,3 +11,7 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif
