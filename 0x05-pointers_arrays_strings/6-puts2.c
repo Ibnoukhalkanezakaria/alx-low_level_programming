bde3dfb6fd774d9ede14ce120ea936b47198601e
ibnoukhalkanezakria <ibnoukhalkanezakaria@gmail.com>
2023-03-27 18:38:25 -0700

    new

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,22 @@
 #include "main.h"
 
 /**
- * puts2 - using loop and putchar
- * putchar
- * @str: string
- */
+*puts2 - using while
+*@str: input
+*Return: void
+*/
+
 void puts2(char *str)
 {
-int i;
+int length = 0;
 
-for (i = 0; str[i] != '\0'; i += 2)
+while (str[length])
 {
-_putchar(str[i]);
+if (length % 2 == 0)
+{
+_putchar(str[length]);
+}
+length++;
 }
 _putchar('\n');
 }
